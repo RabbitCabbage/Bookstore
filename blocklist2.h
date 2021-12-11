1b1e9f43b7c26ce5dc0c6f3107958d967a6c37bf
RabbitCabbage <shen-dong@sjtu.edu.cn>
2021-12-11 13:27:04 +0800

    文件存储部分

--- blocklist2.h
+++ blocklist2.h
@@ -58,7 +58,7 @@ bool compare_char(const char *lhs, const char *rhs) {
 //    if(equal(lhs.first,rhs.first))return true;
 //    else return false;
 //};
-const int length = 500;
+const int length = 50;
 
 
 class BlockList {
