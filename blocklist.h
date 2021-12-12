bdb1571cea25c91fc94ac22bfc8660a20e4a33a0
RabbitCabbage <shen-dong@sjtu.edu.cn>
2021-12-12 09:53:30 +0800

    文件存储部分

--- blocklist.h
+++ blocklist.h
@@ -27,8 +27,8 @@ public:
     struct Block {
         int CurrentSize = 0;
         const int MaxSize = 2000;
-        char first_array[2000][length];
-        int second_array[2000]; //maybe you need bigger array, to do
+        char first_array[2500][length];
+        int second_array[2500]; //maybe you need bigger array, to do
         char MaxValue[length];
         char MinValue[length];
         long Next = -100000;
